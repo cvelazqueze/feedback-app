7f05ef2c1bfaa90c3b05db151b1289f2871132e8
Cesar Velzaquez <cesarve@mty.ipointsystems.com>
2022-10-04 11:58:55 -0500

    Almost complete

--- react/feedback-app/src/App.js
+++ react/feedback-app/src/App.js
@@ -1,28 +1,38 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackStats from "./components/FeedbackStats"
-import FeedbackData from "./data/FeedbackData"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 
-function App(){
-    const [feedback, setFeedback] = useState(FeedbackData)
+function App() {
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
 
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')) {
-            setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
 
-
-    return(
-        <>
-            <Header/> 
-            <div className="container">
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback = {feedback} handleDelete={deleteFeedback}/>
-            </div>
-        </>
-    )
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
 }
 
 export default App
--- react/feedback-app/src/components/FeedbackItem.jsx
+++ react/feedback-app/src/components/FeedbackItem.jsx
@@ -1,17 +1,28 @@
-import { FaTimes } from 'react-icons/fa'
-import Card from "./shared/Card"
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({item, handleDelete}) { 
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
 
   return (
-    <Card className="card">
-        <div className="num-display">{item.rating}</div>
-        <button onClick={() => handleDelete(item.id)} className="close">
-          <FaTimes color='purple'/>
-        </button>
-        <div className="text-display">{item.text}</div>
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
     </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
